78f2362d9ab68a59558f8c6742fcb81448b56e02
carlos79 <carlos.chicaiza2@gmail.com>
2025-09-23 21:17:16 -0500

    feat: add modal services

--- frontend/tcs-challenge/src/app/core/services/modal.service.ts
+++ frontend/tcs-challenge/src/app/core/services/modal.service.ts
@@ -1,9 +1,72 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
+
+export interface ConfirmationModalData {
+  name: string;
+  id: string;
+  showCancelButton?: boolean;
+}
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ModalService {
 
-  constructor() { }
+  private confirmModalState = new Subject<ConfirmationModalData | null>();
+  public confirmModalState$ = this.confirmModalState.asObservable();
+
+  // Este Subject es solo para el modal de ERRORES
+  private errorModalState = new Subject<boolean>();
+  public errorModalState$ = this.errorModalState.asObservable();
+
+  private confirmResponse = new Subject<boolean>();
+  private errorResponse = new Subject<void>();
+
+
+  constructor(private router: Router) { }
+
+  // Método para abrir el modal de CONFIRMACIÓN.
+  public openConfirmationModal(data: ConfirmationModalData): Observable<boolean> {
+    this.confirmModalState.next(data);
+
+    return new Observable(observer => {
+      const sub = this.confirmResponse.subscribe(response => {
+        observer.next(response);
+        observer.complete();
+      });
+
+      return () => {
+        sub.unsubscribe();
+      };
+    });
+  }
+
+  // Método para cerrar el modal de CONFIRMACIÓN y emitir la respuesta del usuario.
+  public closeConfirmationModal(response: boolean): void {
+    this.confirmResponse.next(response);
+    this.confirmModalState.next(null);
+  }
+
+  // Método para abrir el modal de ERRORES.
+  public openErrorModal(): void {
+    this.errorModalState.next(true);
+  }
+
+  // Método simple para cerrar el modal de ERRORES.
+  public closeErrorModal(): void {
+    this.errorModalState.next(false);
+    this.errorResponse.next();
+  }
+
+  public handleBackendError(): void {
+    this.errorModalState.next(true);
+
+    this.errorResponse.subscribe(() => {
+      this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+        this.router.navigate(['/products']);
+      });
+    });
+  }
 }
